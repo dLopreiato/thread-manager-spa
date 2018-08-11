8a90e6dda10e25f48494f3703efbd21d84a0b8d4
Drew Lopreiato <drew@lopreiato.email>
2018-08-11 15:12:26 -0700

    Implement dependency additions

--- app/src/App.js
+++ app/src/App.js
@@ -23,54 +23,76 @@ class App extends Component {
 
   }
 
-  // TODO: Handle reference changes
-  handleThreadUpdate(newThread) {
-    var oldThread = this.state.threads.find(x => x.id === newThread.id);
+  /**
+   * 
+   * @param {Thread} newThread 
+   * 
+   * WARNING: We are mutating objects.
+   *   This is explicitly told not to do in the React documentation but we are doing it anyway. This is because
+   *   this particular field is a reference field. If we were to strictly follow the rules, we would have to either
+   *   use soft references (use the string id, and then perform a look up on every get), or we would have to
+   *   recreate the entire tree. To avoid both of these (admittedly marginal) performance hits, we will be
+   *   modifying references in objects.
+   *  
+   * WARNING: Parameter will have invalid thread tree
+   *   If the user has adjusted any dependencies, the parameter will have an invalid thread tree which must be fixed.
+   */
+  handleThreadUpdate(targetThread) {
+    var oldThread = this.state.threads.find(x => x.id === targetThread.id);
     if (!oldThread) {
-      console.error('this thread does not exist');
-      return;
+      throw new Error('this thread does not exist');
     }
-    else {
-      var newThreads = this.state.threads.slice();
-      for (let i = 0; i < newThreads.length; i += 1) {
-        let relevantThread = newThreads[i];
-        
-        if (relevantThread === oldThread) {
-          newThreads[i] = newThread;
-        }
-        
-        else {
-          // WARNING: WE ARE MUTATING OBJECTS.
-          // This is explicitly told not to do in the React documentation but we are doing it anyway. This is because
-          // this particular field is a reference field. If we were to strictly follow the rules, we would have to either
-          // use soft references (use the string id, and then perform a look up on every get), or we would have to
-          // recreate the entire tree. To avoid both of these (admittedly marginal) performance hits, we will be
-          // modifying references in objects.
 
-          // For clarity on what these two for loops do: We go thread every dependency on this thread (which requires 2
-          // for loops since these are double linked lists), and modify the references of that dependency to point to the
-          // newly cloned thread instead of the old thread.
-          for (let j = 0; j < newThread.dependentOn.length; j += 1) {
-            let otherThread = newThread.dependentOn[j];
-            for (let k = 0; k < otherThread.dependencyOf.length; k += 1) {
-              if (otherThread.dependencyOf[k] === oldThread) {
-                otherThread.dependencyOf[k] = newThread;
-              }
-            }
-          }
+    // We "undo" the changes in the target thread because it has invalid dependencies. We'll fix those later.
+    var newThread = targetThread.clone();
+    newThread.dependentOn = oldThread.dependentOn.slice();
+    newThread.dependencyOf = oldThread.dependencyOf.slice();
 
-          for (let j = 0; j < newThread.dependencyOf.length; j += 1) {
-            let otherThread = newThread.dependencyOf[j];
-            for (let k = 0; k < otherThread.dependentOn.length; k += 1) {
-              if (otherThread.dependentOn[k] === oldThread) {
-                otherThread.dependentOn[k] = newThread;
-              }
-            }
-          }
-        }
+    // WARNING: WE ARE MUTATING OBJECTS.
+    // This is explicitly told not to do in the React documentation but we are doing it anyway. This is because
+    // this particular field is a reference field. If we were to strictly follow the rules, we would have to either
+    // use soft references (use the string id, and then perform a look up on every get), or we would have to
+    // recreate the entire tree. To avoid both of these (admittedly marginal) performance hits, we will be
+    // modifying references in objects.
+    oldThread.swapWith(newThread);
 
-      }
+    // Maintain a list of the latest versions of the changed threads so that we may update the thread manifest
+    let finalChanges = {};
+    finalChanges[oldThread.id] = newThread;
+    
+    let dependentOnAdditions = targetThread.dependentOn.filter(t => !oldThread.dependentOn.includes(t));
+    for (let newDependentOn of dependentOnAdditions) {
+      let newestThread = finalChanges[oldThread.id];
+      let newestDependentOn = finalChanges[newDependentOn.id] ? finalChanges[newDependentOn.id] : newDependentOn;
+
+      let changedThreads = newestThread.addDependency(newestDependentOn);
+
+      newestThread.swapWith(changedThreads.thisReplacement);
+      newestDependentOn.swapWith(changedThreads.parameterReplacement);
+
+      finalChanges[changedThreads.thisReplacement.id] = changedThreads.thisReplacement;
+      finalChanges[changedThreads.parameterReplacement.id] = changedThreads.parameterReplacement;
     }
+
+    let dependencyOfAdditions = targetThread.dependencyOf.filter(t => !oldThread.dependencyOf.includes(t));
+    for (let newDependencyOf of dependencyOfAdditions) {
+      let newestThread = finalChanges[oldThread.id];
+      let newestDependencyOf = finalChanges[newDependencyOf.id] ? finalChanges[newDependencyOf.id] : newDependencyOf;
+
+      let changedThreads = newestDependencyOf.addDependency(newestThread);
+
+      newestDependencyOf.swapWith(changedThreads.thisReplacement);
+      newestThread.swapWith(changedThreads.parameterReplacement);
+
+      finalChanges[changedThreads.thisReplacement.id] = changedThreads.thisReplacement;
+      finalChanges[changedThreads.parameterReplacement.id] = changedThreads.parameterReplacement;
+    }
+
+    // TODO: Handle reference removals
+
+
+    console.log('Final changes', finalChanges);
+    var newThreads = this.state.threads.map(x => finalChanges[x.id] ? finalChanges[x.id] : x);
 
     this.setState({threads: newThreads});
     console.log('thread saved');
@@ -96,6 +118,10 @@ class App extends Component {
    */
   initializeThreads() {
     // for now, prepopulate threads
+    var thread7 = new Thread();
+    thread7.name = 'Independent Thread';
+    thread7.waitingOn = 'Me';
+
     var thread6 = new Thread();
     thread6.name = 'Child Thread that is finished';
     thread6.waitingOn = 'Me';
@@ -135,7 +161,7 @@ class App extends Component {
     thread0.addDependency(thread3);
     thread0.addDependency(thread4);
 
-    return [thread0, thread1, thread2, thread3, thread4, thread5, thread6];
+    return [thread0, thread1, thread2, thread3, thread4, thread5, thread6, thread7];
   }
 
   getThread(id) {
